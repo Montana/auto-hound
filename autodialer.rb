3b5a0748ab4e86b91cba0ff0b808bdd2ce164b9c
Montana Mendy <montanamendy@gmail.com>
2018-04-22 02:10:14 -0700

    Create autodialer.rb

---
+++ autodialer.rb
@@ -0,0 +1,15 @@
+require 'ragi/call_handler'
+class CallerIdHandler < RAGI::CallHandler
+  APP_NAME = 'simon'
+  
+  def identify
+    answer
+    wait(1) 
+
+    user_phone_number = @params[RAGI::CALLERID]
+    
+    say_invoices.rb(user_phone_number)
+  
+    hang_up
+  end
+end
